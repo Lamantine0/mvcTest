6ef5b542077a8f3c4d8f308a80fb232154c7008e
dimatereshko95 <dimatereshko95@gmail.com>
2021-02-08 19:06:31 +0300

    Начало создания mvc, исправление difined на difine ///

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
 // Подключение файлов
-defined('ROOT', dirname(__FILE__));
-require_once(ROOT.'mvcTest/components/Router.php');
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
 
 
 
